68aab3cbd6881cc890a4cde079310dd6d4542752
Patrik Olin <enmejl@gmail.com>
2019-09-12 07:03:39 +0200

    k

--- keyboards/keebio/levinson/keymaps/bakingpy2u/config.h
+++ keyboards/keebio/levinson/keymaps/bakingpy2u/config.h
@@ -3,12 +3,12 @@
 /* Use I2C or Serial, not both */
 
 #define USE_SERIAL
-// #define USE_I2C
+//#define USE_I2C
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #define TAPPING_TERM 150
