31598994309da062678b57d3af2f3d1376f651bd
Patrik Olin <enmejl@gmail.com>
2019-11-22 10:48:17 +0100

    planck_keymap

---
+++ keyboards/keebio/levinson/keymaps/patrikolin/keymap.c
@@ -0,0 +1,183 @@
+#include QMK_KEYBOARD_H
+#include "quantum/keymap_extras/keymap_swedish.h"
+
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _DYN 3
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  LESS,
+  GREATER,
+  DYNAMIC_MACRO_RANGE,
+};
+
+#include "dynamic_macro.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Adjust| Ctrl | Alt  | GUI  |Lower |Space |Space |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = LAYOUT_ortho_4x12( \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SE_QUES, KC_ENT , \
+  KC_LCTL,  KC_LGUI, KC_LALT, KC_BSPC, TT(_LOWER),   KC_SPC,  KC_LEAD,  TT(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |     |    \  |  |   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_LOWER] = LAYOUT_ortho_4x12( \
+    SE_TILD, KC_EXLM, SE_AT,   KC_HASH, SE_DLR,  KC_PERC,  SE_AMPR, SE_CIRC,   KC_GRV,    S(KC_GRV), SE_ASTR, KC_BSPC, \
+    KC_DEL,  SE_SLSH,   SE_LPRN,   SE_RPRN,   SE_APOS,   SE_GRV,   _______,   SE_MINS,    SE_LCBR_MAC,    SE_RCBR_MAC, SE_PLUS, SE_PIPE,\
+    _______, SE_BSLS_MAC,   SE_LCBR,   SE_RCBR,   SE_QUO2,  _______,  _______,  SE_UNDS, SE_LBRC, SE_RBRC, SE_EQL,  S(KC_NUBS),
+    DYN_MACRO_PLAY1,   DYN_MACRO_PLAY2, SE_LBRC, SE_RBRC, KC_TRNS, _______, _______, KC_TRNS,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = LAYOUT_ortho_4x12( \
+  KC_GRV,  KC_F9,    KC_F10,    KC_F11,    KC_F12,    _______,    _______, SE_ASTR,   KC_7,    KC_8,    KC_9,        KC_BSPC, \
+  KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,   _______,   SE_PLUS,   KC_4,    KC_5,    KC_6,        KC_BSLS, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,  _______,  _______,  SE_MINS,  KC_1,     KC_2,    KC_3,     _______, \
+  DYN_REC_START1, DYN_REC_START2, DYN_REC_STOP, _______, KC_TRNS, _______, _______, KC_TRNS, KC_0, _______, _______, _______ \
+),
+
+[_DYN] = LAYOUT_ortho_4x12( \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SE_QUES, KC_ENT , \
+  KC_LCTL,  KC_LGUI, KC_LALT, KC_BSPC, TT(_LOWER),   KC_SPC,  KC_LEAD,  TT(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+
+};
+
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+    // Replace the sequences below with your own sequences.
+    SEQ_ONE_KEY(KC_T) {
+      // When I press KC_LEAD and then T, this sends CTRL + SHIFT + T
+      SEND_STRING(SS_LGUI(SS_LSFT("t")));
+    }
+    SEQ_ONE_KEY(KC_V) {
+      // When I press KC_LEAD and then T, this sends CTRL + SHIFT + T
+      SEND_STRING(SS_LCTRL("v"));
+    }
+    SEQ_ONE_KEY(KC_LEFT) {
+      register_code(KC_LGUI);
+      register_code(KC_LEFT);
+      unregister_code(KC_LEFT);
+      unregister_code(KC_LGUI);
+    }
+    SEQ_ONE_KEY(KC_RGHT) {
+      register_code(KC_LGUI);
+      register_code(KC_RGHT);
+      unregister_code(KC_RGHT);
+      unregister_code(KC_LGUI);
+    }
+    SEQ_ONE_KEY(KC_UP) {
+      register_code(KC_LGUI);
+      register_code(KC_UP);
+      unregister_code(KC_UP);
+      unregister_code(KC_LGUI);
+    }
+    SEQ_ONE_KEY(KC_COMM) {
+      register_code(KC_LGUI);
+      register_code(KC_1);
+      unregister_code(KC_1);
+      unregister_code(KC_LGUI);
+    }
+    SEQ_ONE_KEY(KC_DOT) {
+      register_code(KC_LGUI);
+      register_code(KC_2);
+      unregister_code(KC_2);
+      unregister_code(KC_LGUI);
+    }
+    SEQ_ONE_KEY(SE_QUES) {
+      register_code(KC_LGUI);
+      register_code(KC_3);
+      unregister_code(KC_3);
+      unregister_code(KC_LGUI);
+    }
+    // Note: This is not an array, you don't need to put any commas
+    // or semicolons between sequences.
+    SEQ_TWO_KEYS(KC_N, KC_T) {
+      // When I press KC_LEAD and then N followed by T, this sends CTRL + T
+      SEND_STRING(SS_LGUI("t"));
+    }
+  }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_record_dynamic_macro(keycode, record)) {
+      return false;
+  }
+  switch (keycode) {
+    case GREATER:
+      if (record->event.pressed) {
+        SEND_STRING(">");
+      } else {
+
+      }
+      break;
+          case LESS:
+      if (record->event.pressed) {
+        SEND_STRING("<");
+      } else {
+
+      }
+      break;
+
+  }
+  return true;
+};
+
+
+
---
+++ keyboards/xd75/keymaps/gustavhol/keymap.c
@@ -0,0 +1,136 @@
+/* Copyright 2017 Wunder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "quantum/keymap_extras/keymap_swedish.h"
+// Layer shorthand
+#define _QW 0
+#define _RAISE 1
+#define _LOWER 2
+#define _______ KC_TRNS
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  QMKBEST = SAFE_RANGE,
+  QMKURL
+};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* QWERTY
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * | TAB    | Q      | W      | E      | R      | T      | [      | \      | ]      | Y      | U      | I      | O      | P      | '      |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+ * | CAP LK | A      | S      | D      | F      | G      | HOME   | DEL    | PG UP  | H      | J      | K      | L      | ;      | ENTER  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+ * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | FN     | RALT   | RGUI   | RCTRL  |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
+    KC_ESC,  KC_F1,    KC_2,    C(A(KC_L)),C(S(KC_7)),KC_5,KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_F10,  KC_F11,  KC_F12,  KC_0,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_7,    KC_8,    KC_9,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+    KC_LEAD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_4,    KC_5,    KC_6,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_1,    KC_2,    KC_3,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SFTENT,
+    KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, MO(_RAISE),KC_SPC,KC_BSPC, KC_0,    KC_DEL,  KC_ENTER,  MO(_LOWER),  KC_LEAD, KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+/* RAISE
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * | PREV   | PLAY   | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      |        | RESET  |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * | VOL-   | MUTE   | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        | RGB TG | FN     | RGB RMD| RGB MD | P0     |        | P.     | PENT   | PENT   | FN     |        |        |        |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+  [_RAISE] = LAYOUT_ortho_5x15( /* FUNCTION */
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    _______, SE_UNDS, SE_AMPR, SE_GRV , SE_LCBR, SE_RCBR, KC_P7,   KC_P8,   KC_P9,   SE_LESS, SE_GRTR, SE_EQL , SE_PLUS, _______, _______,
+    _______, SE_MINS , SE_APOS,SE_QUO2, SE_LPRN, SE_RPRN, KC_P4,   KC_P5,   KC_P6,   SE_LBRC, SE_RBRC, SE_COLN, SE_SLSH , SE_AT  , _______,
+    _______, SE_CIRC, SE_BSLS, SE_ASTR, KC_EXLM, S(KC_3), KC_P1,   KC_P2,   KC_P3,   SE_PIPE, SE_QUES, SE_DLR , KC_PERC, SE_TILD, _______,
+    _______, _______, _______, _______, MO(_RAISE),_______, _______,   RESET, _______, _______, MO(_LOWER), _______, _______, _______, _______
+  ),
+  /* LOWER
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * | PREV   | PLAY   | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      |        | RESET  |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * | VOL-   | MUTE   | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        | RGB TG | FN     | RGB RMD| RGB MD | P0     |        | P.     | PENT   | PENT   | FN     |        |        |        |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+  [_LOWER] = LAYOUT_ortho_5x15( /* FUNCTION */
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
+    KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______,
+    KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
+    _______, _______, RGB_TOG, _______, MO(_RAISE),RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, MO(_LOWER), _______, _______, _______, _______
+  )
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QMKBEST:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("QMK is the best thing ever!");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case QMKURL:
+      if (record->event.pressed) {
+        // when keycode QMKURL is pressed
+        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
+      } else {
+        // when keycode QMKURL is released
+      }
+      break;
+  }
+  return true;
+}
+void matrix_init_user(void) {
+}
+LEADER_EXTERNS();
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+    // Replace the sequences below with your own sequences.
+    SEQ_ONE_KEY(KC_T) {
+      // When I press KC_LEAD and then T, this sends CTRL + SHIFT + T
+      SEND_STRING(SS_LGUI(SS_LSFT("t")));
+    }
+    SEQ_ONE_KEY(KC_V) {
+      // When I press KC_LEAD and then T, this sends CTRL + SHIFT + T
+      SEND_STRING(SS_LCTRL("v"));
+    }
+    // Note: This is not an array, you don't need to put any commas
+    // or semicolons between sequences.
+    SEQ_TWO_KEYS(KC_N, KC_T) {
+      // When I press KC_LEAD and then N followed by T, this sends CTRL + T
+      SEND_STRING(SS_LGUI("t"));
+    }
+  }
+}
+void led_set_user(uint8_t usb_led) {
+}
