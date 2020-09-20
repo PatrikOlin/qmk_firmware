1a3b2ed9b2d27f33bb96693f914dbbeaaf79a92f
Patrik Olin <enmejl@gmail.com>
2020-09-20 12:58:20 +0200

    updated kyria keymap

--- keyboards/kyria/keymaps/patrikolin/config.h
+++ keyboards/kyria/keymaps/patrikolin/config.h
@@ -29,6 +29,16 @@
 
 #define MASTER_LEFT
 
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
-// #define NO_USB_STARTUP_CHECK
+#define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
